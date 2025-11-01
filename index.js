332f52c917354639e2f956c5570c57698e04485c
David <davidsaleh03@gmail.com>
2025-11-01 01:30:25 -0700

    figuring out modules

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+//http://api.weatherapi.com/v1/forecast.json?key=af0baaec05d9499b85f41128250111&q=London&days=7&aqi=yes&alerts=yes
+
+
 const accessKey = '538fe67faa9a3be2f8642bc851754629';
 const city = 'Glendale';
-const url = `http://api.weatherstack.com/current?access_key=${accessKey}&query=${encodeURIComponent(city)}`;
+const url1 = `http://api.weatherapi.com/v1/current.json?key=af0baaec05d9499b85f41128250111&q=${city}&aqi=yes`;
+const url2 = `http://api.weatherapi.com/v1/forecast.json?key=af0baaec05d9499b85f41128250111&q=${city}&days=7&aqi=yes&alerts=yes`
 
 async function getInfo() {
-    const info = await fetch(url);
+    const info = await fetch(url1);
+    const info2 = await info.json();
+    console.log(info2);
+}
+
+async function getInfo2() {
+    const info = await fetch(url2);
     const info2 = await info.json();
     console.log(info2);
 }
 
 getInfo();
+getInfo2();
 
 
